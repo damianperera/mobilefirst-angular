c38427ed69b46dc7449dcab92ce85398db49ec0b
Damian Perera <damian@perera.io>
2016-10-01 22:41:40 +0530

    Added Browserify

---
+++ app/main.js
@@ -0,0 +1,4 @@
+var app = require ('./controllers/app.js');
+var directives = require ('./directives/directives.js');
+var factories = require ('./factories/factories.js');
+var routes = require ('./routes/routes.js');
